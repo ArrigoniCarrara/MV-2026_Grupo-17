9c52ff1ba75e222fe1181eb44742ffe5bcb790cf
ArrigoniCarrara <gianlucaarrigoni06@gmail.com>
2026-09-14 23:40:55 -0300

    Vector Tipo_operando y funcion dosOperandos Terminados

--- funciones.c
+++ funciones.c
@@ -1,11 +1,59 @@
 #include "main.c"
 
+uint32_t ninguno(){return 0x0} // ERROR
+
+uint32_t registro(){
+        registros[0] = registros[0] + 1;
+        return RAM[registros[0]];
+}
+
+uint32_t inmediato(){
+    registros[0] = registros[0] + 1;
+
+    uint32_t aux = RAM[registros[0]];
+    registros[0] = registros[0] + 1;
+
+    aux = aux << 8;
+    aux = aux | RAM[registros[0]];
+
+    return aux;
+}
+
+uint32_t memoria(){
+    registros[0] = registros[0] + 1;
+
+    uint32_t aux = RAM[registros[0]];
+    registros[0] = registros[0] + 1;
+
+    aux = aux << 8;
+    aux = aux | RAM[registros[0]];
+    registros[0] = registros[0] + 1;
+
+    aux = aux << 8;
+    aux = aux | RAM[registros[0]];
+
+    return aux;
+}
+
+
+ typedef uint32_t (*tipo_operando[2])(); // Vector de punteros a funciones para rescatar el valor de cada operando
+ tipo_operando p_tipo_op = {ninguno, registro, inmediato, memoria} // y para mover correctamente el IP
+
 void dosOperandos(uint32_t registros[0]){
         unsigned char tipo_opa;
         unsigned char tipo_opb;
-        
-        tipo_opa = registros[0] >> 6;
-        tipo_opb = registros[0] >> 4;
+        unsigned char cod_op;
+        uint32_t valor_opa;
+        uint32_t valor_opb;
+
+        tipo_opb = RAM[registros[0]] >> 6;
+        tipo_opa = RAM[registros[0]] >> 4;
+        cod_op = RAM[registros[0]] & 0x1F;
+
+        valor_opa = p_tipo_op[tipo_opa]
+        valor_opb = p_tipo_op[tipo_opb]
+
+        // TipoOperacion[cod_op](tipoa, tipob, valora, valorb);
 } 
 
 
--- main.c
+++ main.c
@@ -67,6 +67,7 @@ int main(int argc, char *argv[]) {
     registros[27] = tabla_seg[1].base; // DS
     registros[0] = registros[26]; // IP
 
+    // Ciclo Principal
      while(registros[0] != tabla_seg[1].base && registros[0] != -1){
         buscooperacion(registros[0]);
         if (registros[0] != -1)
