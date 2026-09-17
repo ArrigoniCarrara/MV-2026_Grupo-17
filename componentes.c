bfc56553ccfd106005f5382ef4b94afc42c10ff3
nach1i1 <zazpaf@gmail.com>
2026-09-17 00:54:25 -0300

    constantes

--- componentes.c
+++ componentes.c
@@ -2,6 +2,23 @@
 #define TAM_MEMORIA 16384
 #define TAM_SEG 7
 #define CANT_REG 31
+#define IP 0
+#define OPC 1
+#define OP1 2
+#define OP2 3
+#define LAR 4
+#define MAR 5 
+#define MBR 6
+#define EAX 10
+#define EBX 11
+#define ECX 12
+#define EDX 13
+#define EEX 14
+#define EFX 15
+#define AC 16
+#define CC 17
+#define CS 26
+#define DS 27
 
 unsigned char RAM[TAM_MEMORIA]; // Memoria principal
 
@@ -21,3 +38,4 @@ typedef struct{
 tipo_seg tabla_seg[TAM_SEG]; // Tabla de segmentos
 
 uint32_t registros[CANT_REG]; // Registros
+
